24461e86544f09fbaf4b385444aac16eb071f94b
Laura <premillieu.laura@gmail.com>
2015-04-11 15:16:34 +0200

    stringContainer

---
+++ src/util/StringContainer.java
@@ -0,0 +1,18 @@
+package util;
+
+public class StringContainer {
+
+	private String string;
+
+	public StringContainer(String s) {
+		setString(s);
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+}
